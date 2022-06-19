a2c5e00fc8963850d432b1c41dbeda0aaa59fb5c
Nurina Junus <nurina.junus@gmail.com>
2022-06-19 10:17:18 -0600

    add seed data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+User.reset_pk_sequence
+
+u1 = User.create!(name: "Nurina", username: "nurina", password: "1234", image_url: "https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg", role: 0)
+u2 = User.create!(name: "Devin", username: "devin", password: "1234", image_url: "https://www.purina.co.uk/sites/default/files/2020-12/Understanding%20Your%20Cat%27s%20Body%20LanguageHERO.jpg", role: 1)
+u3 = User.create!(name: "Hanna", username: "hanna", password: "1234", image_url: "https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2020-07/kitten-510651.jpg?h=f54c7448&itok=ZhplzyJ9", role: 1)
