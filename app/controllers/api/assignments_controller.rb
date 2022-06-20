80f203d34e7a7851a415fe1b918d27aa24f7ab9c
Nurina Junus <nurina.junus@gmail.com>
2022-06-19 20:13:19 -0600

    Assignments controller actions create show index

--- app/controllers/api/assignments_controller.rb
+++ app/controllers/api/assignments_controller.rb
@@ -1,5 +1,24 @@
-class AssignmentsController < ApplicationController
+class Api::AssignmentsController < ApplicationController
 
-    
+    def create
+        assignment = Assignment.create!(assignment_params)
+        render json: assignment, status: 200
+    end
+
+    def show
+        assignment = Assignment.find(params[:id])
+        render json: assignment, status: 201
+    end
+
+    def index
+        assignments = Assignment.all
+        render json: assignments, status: 201
+    end
+
+    private
+
+    def assignment_params
+        params.permit(:name, :total_points, :graded_points, :student_id, :teacher_id, :comments)
+    end
 
 end
