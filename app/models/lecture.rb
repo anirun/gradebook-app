35ede85574245bf12291f8b43a90a5bf410508df
Nurina Junus <nurina.junus@gmail.com>
2022-06-20 12:11:37 -0600

    Lecture model validations

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,5 +1,9 @@
 class Lecture < ApplicationRecord
     belongs_to :teacher, class_name: "User"
-    has_many :students, class_name: "User"
-    
+    has_many :assignments, dependent: :destroy
+    has_many :students, through: :assignments
+
+    validates :name, presence: true
+    validates :grade, presence: true, numericality: {greater_than_or_equal_to: 6, less_than_or_equal_to: 8}
+
 end
